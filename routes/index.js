e2f314a1f290edbcd89cd09496f0fdfc28f9fff5
Daniel Ramos <danielramos@Daniels-MacBook-Pro-2.local>
2018-02-21 23:18:52 -0500

    - adding the routes all the pages for the portfolio on index.js file

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET About Me Page. */
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About Me' });
+});
+
+/* GET Projects Page. */
+router.get('/projects', (req, res, next) => {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* GET Services Page. */
+router.get('/services', (req, res, next) => {
+  res.render('services', { title: 'Services' });
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/contact', (req, res, next) => {
+  res.render('contact', { title: 'Contact Me' });
+});
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
